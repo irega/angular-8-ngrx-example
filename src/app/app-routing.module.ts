96c04feaa7e6ac763c69bc73663ea88d6eaa6d5f
irega <ivan.reinoso@outlook.es>
2019-07-13 00:49:16 +0200

    added @ngrx/store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'my-counter', component: MyCounterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
